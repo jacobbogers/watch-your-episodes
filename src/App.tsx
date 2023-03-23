58d54cddd9520bf2ebeebf9ee8640834c554f523
Jacob Bogers <jkfbogers@gmail.com>
2023-03-23 15:38:48 +0100

    remormatted the main grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import styles from './App.module.scss';
 
 import Header from './components/Header';
+import Main from './components/Main';
 import HomeNavigation from './components/HomeNavigation';
 
 export default function App() {
@@ -8,6 +9,7 @@ export default function App() {
         <div className={styles.app}>
             <Header />
             <HomeNavigation />
+            <Main />
         </div>
     );
 }
---
+++ src/components/Main/index.tsx
@@ -0,0 +1,11 @@
+import type { PropsWithChildren } from 'react';
+
+import styles from './main.module.scss';
+
+type MainProperties = PropsWithChildren<Record<never, never>>;
+
+export default function Main(props: MainProperties){
+
+    return <main className={styles.main_section}>{props.children}
+    </main>
+}
